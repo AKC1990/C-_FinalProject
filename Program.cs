c718e8047c09b1e704167952595d26ef7f788dee
Kirill Antropov <chs4t-740@mail.ru>
2022-12-08 14:32:01 +0300

    Fixed bugs, finished code

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ int CountOfElements(string[] array, int n)
     int count = 0;
     for (int i = 0; i < array.Length; i++)
     {
-        if (array[i] <= n) count += 1;
+        if (array[i].Length <= n) count += 1;
     }
     return count;
 }
@@ -22,39 +22,39 @@ string[] CreateNewArray(int Length)
 }
 
 // Метод, заполняющий массив элементами определенный длины
-string[] FillArray(string[] array, string[] newArray)
+string[] FillArray(string[] array, string[] newArray, int n)
 {
-    for (int i = 0; i < array.Length; i++)
-     {
-        for (int j = 0; j < newArray.Length;)
+    for (int i = 0; i < newArray.Length;)
+    {
+        for (int j = 0; j < array.Length; j++)
         {
-            if (array[i] <= n) 
+            if (array[j].Length <= n) 
             {
-                newArray[j] = array[i];
-                j++;
+                newArray[i] = array[j];
+                i++;
             }
-
         }
     }
     return newArray;
 }
 
 // Метод, печатающий новый массив
-string[] PrintArray(string[] array)
+string PrintArray(string[] array)
 {
     string result = "[ ";
     for (int i = 0; i < array.Length; i++)
     {
-        result += $"{array[i]} ";
+        result += $"{array[i]}";
+        if (i < array.Length - 1) result += ", ";
     }
-    return result + "]";
+    return result + " ]";
 }
 
 // Клиентский код
-string[] myArray = {"Hello", "2", "world", ":-)"};
+string[] myArray = { "1234", "2", "computer science", "-2", ":-)" };
 int n = 3;
-string[] newArray = FillArray(myArray, CreateNewArray(CountOfElements(myArray, n)));
-Console.WriteLine(PrintArray(newArray));
+string[] newArray = CreateNewArray(CountOfElements(myArray, n));
+Console.WriteLine(PrintArray(FillArray(myArray, newArray, n)));
 
 
 
